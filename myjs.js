3c0bdfeaaee407d1d0dd21281ea76160f3d352cd
Sudhanshu Shekhar <91026313+sud516@users.noreply.github.com>
2022-02-27 19:58:14 +0530

    v1

---
+++ myjs.js
@@ -0,0 +1,11 @@
+document.addEventListener("click", () => {
+  const newColor = makeNewColor();
+  document.body.style.backgroundColor = newColor;
+});
+
+function makeNewColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r},${g},${b})`;
+}
